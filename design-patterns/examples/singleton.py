77224b6c88ba7287744721bd044c8f60e9bad13d
Bart Rosa <contact@bartrosa.dev>
2024-05-28 14:10:58 +0200

    Update singleton with mp and mt

--- design-patterns/examples/singleton.py
+++ design-patterns/examples/singleton.py
@@ -1,4 +1,5 @@
-# Class method implementation - single thread
+###############################################################################
+# Class implementation - single thread
 
 class Singleton:
     _instance = {}
@@ -8,7 +9,7 @@ class Singleton:
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
-
+###############################################################################
 # Decorator implementation - single thread
 
 def singleton(cls):
@@ -35,3 +36,81 @@ if __name__ == "__main__":
     db1 = Database()
     db2 = Database()
     print(db1 is db2)
+
+###############################################################################
+# Decorator implementation - multithread
+
+import threading
+
+def singleton(cls):
+    instances = {}
+    lock = threading.Lock()
+    
+    def get_instance(*args, **kwargs):
+        nonlocal instances
+        if cls not in instances:
+            with lock:
+                if cls not in instances:  # Double-checked locking
+                    instances[cls] = cls(*args, **kwargs)
+        return instances[cls]
+    
+    return get_instance
+
+@singleton
+class SingletonClass:
+    def __init__(self):
+        self.value = 42
+
+    def get_value(self):
+        return self.value
+
+def singleton_test():
+    instance = SingletonClass()
+    print(f"Instance ID: {id(instance)}, Value: {instance.get_value()}")
+
+threads = []
+for i in range(10):
+    t = threading.Thread(target=singleton_test)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+    
+###############################################################################
+# Class implementation - multiprocessing
+
+from multiprocessing import Manager, Process, Lock
+
+class SingletonMeta(type):
+    _instances = {}
+    _lock = Lock()
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            with cls._lock:
+                if cls not in cls._instances:
+                    manager = Manager()
+                    cls._instances[cls] = manager.SingletonClass()
+        return cls._instances[cls]
+
+class SingletonClass(metaclass=SingletonMeta):
+    def __init__(self):
+        self.value = 42
+
+    def get_value(self):
+        return self.value
+
+def singleton_test():
+    instance = SingletonClass()
+    print(f"Instance ID: {id(instance)}, Value: {instance.get_value()}")
+
+if __name__ == "__main__":
+    processes = []
+    for i in range(10):
+        p = Process(target=singleton_test)
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
