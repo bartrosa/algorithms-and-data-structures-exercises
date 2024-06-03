b59fde478ae2f22cb849b00adced715e8de3f20d
Bart Rosa <contact@bartrosa.dev>
2024-06-03 09:08:32 +0200

    Update cheatsheet

--- basics/cheatsheet.py
+++ basics/cheatsheet.py
@@ -558,6 +558,46 @@ for num in counter:
 ###############################################################################
 # K
 ###############################################################################
+# Keywords      protected words in python.
+
+    # False
+    # None
+    # True
+    # and
+    # as
+    # assert
+    # async
+    # await
+    # break
+    # class
+    # continue
+    # def
+    # del
+    # elif
+    # else
+    # except
+    # finally
+    # for
+    # from
+    # global
+    # if
+    # import
+    # in
+    # is
+    # lambda
+    # nonlocal
+    # not
+    # or
+    # pass
+    # raise
+    # return
+    # try
+    # while
+    # with
+    # yield
+    # peg_parser
+    # match
+    # case
 
 ###############################################################################
 # L
